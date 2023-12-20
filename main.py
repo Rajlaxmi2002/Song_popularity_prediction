4a6e95e329e8598359aec8b4f164abab506d51e8
Rajlaxmi2002 <rajlaxmipatilsarita@gmail.com>
2023-12-20 21:38:25 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.SongPopularityPrediction import logger
+from src.SongPopularityPrediction.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome")
+STAGE_NAME="Data Ingestion stage"
+
+try:
+    logger.info("STAGE : {STAGE_NAME} started")
+    data_ing=DataIngestionTrainingPipeline()
+    data_ing.main()
+    logger.info("STAGE : {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
